a1cc8189af49a201c621d38b780688913f4c8fe1
Eliot Barrabes <eliot.barrabes@gmail.com>
2024-12-06 16:21:11 +0100

    -Fixed connection to database not working

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,7 +1,7 @@
 from typing import List
-# import mariadb
-# import sys
-import mysql.connector
+import mariadb
+import sys
+# import mysql.connector
 from ConfigManager import ConfigManager
 
 
@@ -9,29 +9,158 @@ class DatabaseManager:
 
     def __init__(self):
         # data retrieve queries
-        self.get_dbs_query = 'SHOW DATABASES'
-        self.get_tables_query = "SHOW TABLES"
-        self.get_fields_query = "SHOW FIELDS FROM %t"
-        self.get_records_query = "SELECT ? FROM ?"
+        self._get_dbs_query = 'SHOW DATABASES'
+        self._get_tables_query = "SHOW TABLES"
+        self._get_fields_query = "SHOW FIELDS FROM %t"
+        self._get_records_query = "SELECT ? FROM ?"
 
         # data insertion queries
-        self.records_insert_query = "INSERT INTO ? (?) VALUES(?)"
-        self.table_creation_query = """CREATE TABLE ? ()"""  # https://www.w3schools.com/sql/sql_foreignkey.asp
-        self.database_creation_query = "CREATE DATABASE ?"
+        self._records_insert_query = "INSERT INTO ? (?) VALUES(?)"
+        self._table_creation_query = """CREATE TABLE ? ()"""  # https://www.w3schools.com/sql/sql_foreignkey.asp
+        self._database_creation_query = "CREATE DATABASE ?"
 
-        self.config_manager = ConfigManager()
-        self.connection = mysql.connector.connect(
-            host=self.config_manager.host,
-            user=self.config_manager.user,
-            password=self.config_manager.password,
-            database=self.config_manager.database
+        self._config_manager = ConfigManager()
+        # self._connection = mariadb.connect(
+        #     host=self._config_manager.host,
+        #     user=self._config_manager.user,
+        #     password=self._config_manager.password,
+        #     database=self._config_manager.database
+        # )
+        self._connection = mariadb.connect(
+            host=self._config_manager.host,
+            user=self._config_manager.user,
+            password=self._config_manager.password,
+            database=self._config_manager.database
         )
+        self._cursor = None
+        # self._connection.close()
 
-    def getAll(self, fields: List[str], table: str):
-        pass
+    # def getAll(self, fields: List[str], table: str):
+    #     pass
 
-    def Get(self, query: str, *args) -> list:
-        pass
+    # def Get(self, query: str, *args) -> list:
+    #     self._connection.reconnect()
+    #
+    #     self._cursor = self._connection.cursor()
+    #
+    #     print(args)
+    #
+    #     self._connection.close()
+    #     self._cursor = None
+    #     return []
+
+    def _getAllTables(self) -> List[tuple]:
+        print(self._connection.open)
+        self._connection.reconnect()
+        self._cursor = self._connection.cursor()
+
+        self._cursor.execute(self._get_tables_query)
+        tables = self._cursor.fetchall()
+
+        self._connection.close()
+        self._cursor = None
+        return tables
+
+    def _getFieldsFromTable(self, table: str) -> List[tuple]:
+        self._connection.reconnect()
+        self._cursor = self._connection.cursor()
+
+        self._cursor.execute(self._get_fields_query, table)
+        fields = self._cursor.fetchall()
+
+        self._connection.close()
+        self._cursor = None
+
+        return fields
+
+    def _getRecordsFromTable(self, fields: list, table: str) -> List[tuple]:
+        self._connection.reconnect()
+        self._cursor = self._connection.cursor()
+
+        self._cursor.execute(self._get_fields_query, (fields, table))
+        records = self._cursor.fetchall()
+
+        self._connection.close()
+        self._cursor = None
+
+        return records
+
+
+#
+# class DatabaseManager:
+#
+#     def __init__(self):
+#         # data retrieve queries
+#         self._get_dbs_query = 'SHOW DATABASES'
+#         self._get_tables_query = "SHOW TABLES"
+#         self._get_fields_query = "SHOW FIELDS FROM %t"
+#         self._get_records_query = "SELECT ? FROM ?"
+#
+#         # data insertion queries
+#         self._records_insert_query = "INSERT INTO ? (?) VALUES(?)"
+#         self._table_creation_query = """CREATE TABLE ? ()"""  # https://www.w3schools.com/sql/sql_foreignkey.asp
+#         self._database_creation_query = "CREATE DATABASE ?"
+#
+#         self._config_manager = ConfigManager()
+#         self._connection = mysql.connector.connect(
+#             host=self._config_manager.host,
+#             user=self._config_manager.user,
+#             password=self._config_manager.password,
+#             database=self._config_manager.database
+#         )
+#         self._cursor = None
+#         self._connection.close()
+#
+#     def getAll(self, fields: List[str], table: str):
+#         pass
+#
+#     def Get(self, query: str, *args) -> list:
+#         self._connection.reconnect()
+#
+#         self._cursor = self._connection.cursor()
+#
+#         print(args)
+#
+#         self._connection.close()
+#         self._cursor = None
+#         return []
+#
+#     def _getAllTables(self) -> List[tuple]:
+#         self._connection.reconnect()
+#         self._cursor = self._connection.cursor()
+#
+#         self._cursor.execute(self._get_tables_query)
+#         tables = self._cursor.fetchall()
+#
+#         self._connection.close()
+#         self._cursor = None
+#         return tables
+#
+#     def _getFieldsFromTable(self, table: str) -> List[tuple]:
+#         self._connection.reconnect()
+#         self._cursor = self._connection.cursor()
+#
+#         self._cursor.execute(self._get_fields_query, table)
+#         fields = self._cursor.fetchall()
+#
+#         self._connection.close()
+#         self._cursor = None
+#
+#         return fields
+#
+#     def _getRecordsFromTable(self, fields: list, table: str) -> List[tuple]:
+#         self._connection.reconnect()
+#         self._cursor = self._connection.cursor()
+#
+#         self._cursor.execute(self._get_fields_query, (fields, table))
+#         records = self._cursor.fetchall()
+#
+#         self._connection.close()
+#         self._cursor = None
+#
+#         return records
+
+
 
 
 # try:
@@ -59,3 +188,9 @@ class DatabaseManager:
 # https://realpython.com/python-mysql/
 
 #bts@siec.education.fr
+
+
+
+
+
+
