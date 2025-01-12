82d7ac6caa969fd5eebf44dc75415590a8f5c9d6
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-01-12 19:17:15 +0100

    - Removed some method in Cypher class because they weren't useful here

--- Cypher.py
+++ Cypher.py
@@ -1,24 +1,23 @@
+import os
+
 from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
+os.environ['ENC_KEY'] = str(key)
+
+# print(os.environ['ENC_KEY'])
+
+
 class Cypher:
 
-    def encryptDatabaseContent(self, database_content) -> dict:
-        encrypted_db_content: dict = {}
-        pass
-
-    def decryptDatabaseContent(self, database_content) -> dict:
-        encrypted_db_content: dict = {}
-        pass
-
-    def _encryptString(self, string: str) -> str:
+    def encryptString(self, string: str) -> str:
         # print(key.decode())
         fernet = Fernet(key)
         byte_string = str.encode(string)
         # print(fernet.encrypt(byte_string))
         return str(fernet.encrypt(byte_string))
 
-    def _decryptString(self, string: str) -> str: # todo fusionner encrypt et decrypt dans la meme methode
+    def decryptString(self, string: str) -> str: # todo fusionner encrypt et decrypt dans la meme methode
         fernet = Fernet(key)
         byte_string = str.encode(string)
         # print(fernet.encrypt(byte_string))
