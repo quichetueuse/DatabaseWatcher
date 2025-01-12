a7db17a297a8c07a44889c07f844c16599693420
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-01-12 13:33:04 +0100

    - Added method that turn json dump into a json string (data can be encrypted or not)

--- DumpManager.py
+++ DumpManager.py
@@ -1,21 +1,96 @@
 import json
+# from json import dumps
+from typing import List
 
+from ConfigManager import ConfigManager
+from Cypher import Cypher
+from json import dumps, dump
 
 class DumpManager:
 
-    def _toJson(self, database_content: dict, raw: bool = False) -> str:
+    def __init__(self, config_manager: ConfigManager):
+        self.config_manager = config_manager
+
+        self.crypt_dump = self.config_manager.crypt_dump
+
+        if self.crypt_dump:
+            self.cypher = Cypher()
+        else:
+            self.cypher = None
+
+    def toJson(self, database_dump: dict, encrypt_data: bool = False) -> str:
         """
         Method that take à dictionary and convert it to a string in json format
-        :param database_content: Content of the database
-        :param raw: If true json values are not encrypted
+        :param database_dump: Content of the database
+        :param encrypt_data: If true json values are not encrypted
         :rtype: str
         :return: Content of the database is json format (in string)
         """
 
-        #crypt
+        # If no encryption is needed
+        if not encrypt_data:
+            return json.dumps(database_dump, indent=4, ensure_ascii=False)
+
+        # if encryption is needed
+
+        # Dictionary that will store encrypted data
+        encrypted_database_dump: dict = {"tables": []}
+
+        tables = database_dump.get('tables')
+        # Loop through every table
+        for table in tables:
+
+            # Blank pattern for database data
+            table_pattern = {"name": None, "fields": [], "records": []}
+
+            # Filling pattern with encrypted data
+            table_pattern['name'] = self.cypher.encryptString(table['name'])
+            table_pattern['fields'] = self._encryptFields(table['fields'])
+            table_pattern['records'] = self._encryptRecords(table['records'])
+            # print(self.cypher.encryptString(table['name']))
+
+            encrypted_database_dump['tables'].append(table_pattern)
+
+        return json.dumps(encrypted_database_dump, indent=4, ensure_ascii=False)
+
+    def _encryptFields(self, fields: List[dict]) -> List[dict]:
+
+        # List that will store all the field patterns
+        field_patterns: list[dict] = []
+
+        for field in fields:
+            # Getting a copy of field pattern
+            field_pattern = {"field": None, "type": None, "null": True, "key": None, "default": None, "extra": None}
+
+            # Filling blank pattern
+            for key in field_pattern.keys():
+                field_pattern[key] = self.cypher.encryptString(str(field[key]))
+
+            # Appending pattern to pattern list
+            field_patterns.append(field_pattern)
+
+        return field_patterns
+
+    def _encryptRecords(self, records: List[list]) -> List[list]:
+
+        # List of tuple that will store every record
+        record_values: List[list] = []
+
+        # looping through each record fetched from database
+        for record in records:
+            # Convert every value to string to avoid exception while trying to convert datetime to json
+            temporary = [self.cypher.encryptString(str(value)) for value in record]
+            record_values.append(temporary)
+
+        return record_values
+
+    def _writeToFile(self, json_string: str):
         pass
 
     def _toString(self, database_content: dict, raw: bool = False):
+        pass
+
+    def createBackupFile(self, database_dump: dict):
         pass
 
     def backupDatabase(self, databse_name: str):
