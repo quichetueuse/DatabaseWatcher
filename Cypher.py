ecc3369814c30d277186a37366a45735945db42d
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-03-08 15:42:33 +0100

    - Fixed cypher decrypt and encrypt methods (not properly converting bytes to string)  - Env var is now created each time a cypher is created

--- Cypher.py
+++ Cypher.py
@@ -2,26 +2,37 @@ import os
 
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()
-os.environ['ENC_KEY'] = str(key)
-
+# print(os.environ)
+#
+# encrypt_key: str = os.environ.get('ENC_kEY')
+# if encrypt_key is None or encrypt_key == "":
+#     key = Fernet.generate_key()
+#     os.environ['ENC_KEY'] = str(key)
+# print("current encrypt key", os.environ['ENC_kEY'])
 # print(os.environ['ENC_KEY'])
 
 
 class Cypher:
 
+    def __init__(self):
+        encrypt_key: str = os.environ.get('ENC_kEY')
+        if encrypt_key is None or encrypt_key == "":
+            self.key = Fernet.generate_key()
+            os.environ['ENC_KEY'] = self.key.decode()
+        print("current encrypt key", os.environ['ENC_kEY'])
+
     def encryptString(self, string: str) -> str:
         # print(key.decode())
-        fernet = Fernet(key)
+        fernet = Fernet(self.key)
         byte_string = str.encode(string)
         # print(fernet.encrypt(byte_string))
-        return str(fernet.encrypt(byte_string))
+        return fernet.encrypt(byte_string).decode()
 
     def decryptString(self, string: str) -> str: # todo fusionner encrypt et decrypt dans la meme methode
-        fernet = Fernet(key)
+        fernet = Fernet(self.key)
         byte_string = str.encode(string)
         # print(fernet.encrypt(byte_string))
-        return str(fernet.decrypt(byte_string))
+        return fernet.decrypt(byte_string).decode()
 
 
 #https://www.tutorialspoint.com/cryptography_with_python/cryptography_with_python_quick_guide.htm
