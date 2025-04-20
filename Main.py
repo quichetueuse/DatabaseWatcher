2be0ed62e3e4e583ba5275680b3f22aef7e29546
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-04-20 12:04:02 +0200

    - Reworked main file

--- Main.py
+++ Main.py
@@ -1,5 +1,26 @@
+import colorama
+
+from DatabaseWatcher import DatabaseWatcher
+import time
 
 
+def backupDatabase():
+    database_watcher = DatabaseWatcher("Tests/config_tests.ini")
+    database_watcher.dumpDatabase()
 
-class Main:
-    pass
+
+def readFromBackup():
+    database_watcher = DatabaseWatcher("Tests/config_tests.ini")
+    database_watcher.compareTables()
+
+
+if __name__ == "__main__":
+    print(colorama.Fore.RED + "Message in this color give information about an error" + colorama.Fore.RESET)
+    print(colorama.Fore.BLUE + "Message in this color give useful informations" + colorama.Fore.RESET)
+    print("\n")
+    start_time = time.time()
+    # Backup database
+    # backupDatabase()
+    # Create database from backuo
+    readFromBackup()
+    print("--- The program ran in %s seconds ---" % round((time.time() - start_time), 3))
